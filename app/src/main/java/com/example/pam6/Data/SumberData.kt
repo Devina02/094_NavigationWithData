a2da1ad4d830d22cbcf42ffdbdb12601bc6f4899
Devina02 <devina.fitriapngs@gmail.com>
2023-11-04 21:57:00 +0700

    first commit

---
+++ app/src/main/java/com/example/pam6/Data/SumberData.kt
@@ -0,0 +1,13 @@
+package com.example.pam6.Data
+
+import com.example.pam6.R
+
+object SumberData {
+    val flavors = listOf(
+        R.string.natural,
+        R.string.vanilla,
+        R.string.lemon,
+        R.string.milk,
+        R.string.lyche,
+    )
+}
