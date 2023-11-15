6b717f8e5dc77018c777503ab2ba9ac6365d7ade
Devina02 <devinafitriapngs@gmail.com>
2023-11-15 21:54:57 +0700

    menambahkan var di orderuistate

--- app/src/main/java/com/example/pam6/Data/OrderUIState.kt
+++ app/src/main/java/com/example/pam6/Data/OrderUIState.kt
@@ -4,4 +4,8 @@ data class OrderUIState(
     val jumlah: Int = 0,
     val rasa: String = "",
     val harga: String = "",
+    var nama: String= "",
+    var alamat: String= "",
+    var tlp: String= ""
+
 )
