65cf277e61ea098fea9c3c2d4202eeb9cbe01596
Devina02 <devinafitriapngs@gmail.com>
2023-11-15 22:03:10 +0700

    Membuat  column

--- app/src/main/java/com/example/pam6/Halamanform.kt
+++ app/src/main/java/com/example/pam6/Halamanform.kt
@@ -1,13 +1,23 @@
 package com.example.pam6
 
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
 import androidx.compose.material3.Button
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.OutlinedTextField
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
-
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.unit.dp
 
 
 @OptIn(ExperimentalMaterial3Api::class)
@@ -25,4 +35,12 @@ fun HalamanSatu(
         mutableStateOf("")
     }
     var listData: MutableList<String> = mutableListOf(namaTxt,alamatTxt,tlpnTxt)
+
+
+    Column (
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.Center,
+        modifier = Modifier.fillMaxSize()
+    ) {}
 }
+
