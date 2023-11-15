889069d7e0f7a48e609c06c8117b626c071e7456
Devina02 <devinafitriapngs@gmail.com>
2023-11-15 22:02:16 +0700

    menambahkan var nama,alamat,telpon dan listdata

--- app/src/main/java/com/example/pam6/Halamanform.kt
+++ app/src/main/java/com/example/pam6/Halamanform.kt
@@ -1,10 +1,28 @@
 package com.example.pam6
 
+import androidx.compose.material3.Button
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 
 
+
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
-Fun Halamanform(
-
-)
+fun HalamanSatu(
+    onSubmitButtonClicked: (MutableList<String>)-> Unit
+){
+    var namaTxt by remember {
+        mutableStateOf("")
+    }
+    var alamatTxt by remember {
+        mutableStateOf("")
+    }
+    var tlpnTxt by remember {
+        mutableStateOf("")
+    }
+    var listData: MutableList<String> = mutableListOf(namaTxt,alamatTxt,tlpnTxt)
+}
