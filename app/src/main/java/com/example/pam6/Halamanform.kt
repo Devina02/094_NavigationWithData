79b9825f7a3f5774c24e9e37f3cb5c76198f25b4
Devina02 <devinafitriapngs@gmail.com>
2023-11-15 22:41:46 +0700

    Menambahkan onCancelButtonClicked dan wrap button with row

--- app/src/main/java/com/example/pam6/Halamanform.kt
+++ app/src/main/java/com/example/pam6/Halamanform.kt
@@ -2,11 +2,15 @@ package com.example.pam6
 
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Button
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.OutlinedButton
 import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
@@ -16,6 +20,7 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.dimensionResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.unit.dp
 
@@ -23,7 +28,8 @@ import androidx.compose.ui.unit.dp
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun Halamanform(
-    onSubmitButtonClicked: (MutableList<String>)-> Unit
+    onSubmitButtonClicked: (MutableList<String>)-> Unit,
+    onCancelButtonClicked: () -> Unit,
 ){
     var namaTxt by remember {
         mutableStateOf("")
@@ -58,8 +64,19 @@ fun Halamanform(
             label = { Text(text = stringResource(id = R.string.notelp)) }
         )
         Spacer(modifier = Modifier.height(15.dp))
-        Button(onClick = {onSubmitButtonClicked(listData)}) {
-            Text(text = stringResource(id = R.string.btnsubmit))
+        Row (modifier = Modifier
+            .fillMaxWidth()
+            .padding(dimensionResource(R.dimen.padding_medium))
+            .weight(1f, false),
+            horizontalArrangement =
+            Arrangement.spacedBy(dimensionResource(R.dimen.padding_medium)),
+            verticalAlignment = Alignment.Bottom){
+            OutlinedButton(modifier = Modifier.weight(1f), onClick = onCancelButtonClicked) {
+                Text(stringResource(R.string.back_button))
+            }
+            Button(modifier = Modifier.weight(1f), onClick = { onSubmitButtonClicked(listData) }) {
+                Text(text = stringResource(id = R.string.btnsubmit))
+            }
         }
     }
 }
